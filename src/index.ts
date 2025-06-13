5626c9c0919762ce6798c787b6e2621afab4a26e
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-13 21:18:42 +0200

    Add /api/words route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import wordRoutes from './routes/wordRoutes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+app.use('/api/words', wordRoutes);
 
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
