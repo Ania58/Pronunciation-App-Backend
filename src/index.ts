b0f350e4d1d09b542c06292cca897390b4c71404
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-14 18:45:33 +0200

    Add backend error handling

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ app.use(express.json());
 
 app.use('/api/words', wordRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Server error", error: err.message });
+});
+
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
