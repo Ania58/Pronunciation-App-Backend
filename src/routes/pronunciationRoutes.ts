c45c0dc3066b2e52f3b804aa2603aeddb63b0205
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-07-08 23:53:44 +0200

    Fix: Move /user/attempts above dynamic :id routes in pronunciationRoutes to avoid incorrect matching

--- src/routes/pronunciationRoutes.ts
+++ src/routes/pronunciationRoutes.ts
@@ -3,11 +3,11 @@ import { submitPronunciationAttempt, getPronunciationAttempts, updatePronunciati
 
 const router = Router();
 
+router.get('/user/attempts', getUserPronunciationAttempts);
 router.post('/:id', submitPronunciationAttempt);
 router.get('/:id/attempts', getPronunciationAttempts);
 router.patch('/:id/feedback', updatePronunciationFeedback);
 router.delete('/:id', deletePronunciationAttempt);
-router.get('/user/attempts', getUserPronunciationAttempts);
 router.post('/:id/transcribe', transcribePronunciation);
 
 
